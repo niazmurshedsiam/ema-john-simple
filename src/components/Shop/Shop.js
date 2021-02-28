9b747858777a69575b7f3a51cd1dc27cac478dc1
niazmurshedsiam <niazmurshed16@gmail.com>
2021-02-28 21:22:37 +0600

    add css style

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,17 +1,25 @@
 import {useState,React} from 'react';
 import fakeData from './../../fakeData/index';
+import './Shop.css';
 
 const Shop = () => {
     
     const first10 = fakeData.slice(0,10);
     const [products,setProducts] = useState(first10);
     return (
-        <div>
-            <ul>
-                {
-                    products.map (product=><li>{product.name}</li>)
-                }
-            </ul>
+        <div className="shop-container">
+            <div className="product-container">
+                <ul>
+                    {
+                        products.map (product=><li>{product.name}</li>)
+                    }
+                </ul>
+                
+            </div>
+            <div className="shop-container">
+                <h3>This is Card Container</h3>
+            </div>
+            
         </div>
     );
 };
